a2a9459a1c6667766ea6203b16f20170b433e25b
FingertipRhythm <23181624229@qq.com>
2020-10-23 11:31:42 +0800

    第二天关于Scanner的测试

---
+++ src/day02/ScannerTest.java
@@ -0,0 +1,14 @@
+package day02;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int nextInt = scan.nextInt();
+		System.out.println(nextInt);
+
+	}
+
+}
